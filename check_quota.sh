9c211becf236c9042b7796e392cdb7e3479cd53f
qjtdsqqm94akkyysgjdqo3hx1jn6l17 <18874@student.vgu.edu.vn>
2025-06-16 17:23:55 +0700

    initial commit: added the script files

---
+++ check_quota.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+### Script to make checking quota easier
+### Usage: check_quota.sh
+
+## MIT License
+##
+## Copyright (c) 2025 18874studentvgu
+##
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this software and associated documentation files (the "Software"), to deal
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+##
+## The above copyright notice and this permission notice shall be included in all
+## copies or substantial portions of the Software.
+##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+
+
+to_human_size(){
+    # numfmt counts in bytes while APPARENLY our file commands count in KiB by default??
+    if which numfmt 1>/dev/null 2>/dev/null; then
+        numfmt --to=iec-i --suffix='B' --from-unit=1024 "$1"
+    else # just make it to MiB and call it a day I don't care
+        echo "$(expr $1 / 1024)MiB" # 1048576 == 1024 ** 2
+   fi
+}
+
+set -- $(quota -wvpu | tail -n 1 | tr -s ' ' | cut -d ' ' -f 2,3,5)
+
+if [ -n "$*" ]; then
+    Q_USED="${1%\*}"
+    Q_LIMIT="$2"
+    Q_GRACE="$3"
+
+    if [ "$Q_LIMIT" -eq 0 ]; then
+        echo "you have no disk quota. lucky you..."
+        echo ''
+    else
+        Q_PERCENT="$(echo "$Q_USED*100/$Q_LIMIT"|bc)"
+        if [ "$Q_PERCENT" -lt "60"  ]; then
+            _f_pfx='\033[1m\033[32m'
+        elif [ "$Q_PERCENT" -lt "90"  ]; then
+       	    _f_pfx='\033[1m\033[33m'
+            extra_message="You might wanna start looking into deleting stuff you don't need."
+       	elif [ "$Q_PERCENT" -lt "100"  ]; then
+            _f_pfx='\033[1m\033[31m'
+       	    extra_message="You REALLY should start deleting stuff about now..."
+        else
+            _f_pfx='\033[1m\033[31m\033[5m'
+            _date_fstring="$(printf '\033[7m%s\033[27m' "$(date -d "@$Q_GRACE" +'%^c VN time (UTC+07)')")"
+            extra_message="I am no longer asking. You have a grace up to $_date_fstring. Delete. Now."
+        fi
+
+        printf "You have used $_f_pfx%s\033[0m ($_f_pfx%s%%\033[0m) of your \033[1m%s\033[0m disk quota.\n\033[2m$_f_pfx\033[25m%s\033[0m\n"\
+            "$(to_human_size "$Q_USED")" "$(echo "$Q_USED*100/$Q_LIMIT"|bc)"\
+            "$(to_human_size "$Q_LIMIT")" "$extra_message"
+    fi
+else 
+    echo "$0: Could not get quota information for current user." >&2
+    echo ''
+fi
+
+unset to_human_size Q_USED Q_LIMIT Q_GRACE _f_pfx extra_message _date_fstring
+
---
+++ fancy-ify.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+### Make things looks fancy smancy
+### Usage: drop it in /etc/profile.d
+
+## MIT License
+##
+## Copyright (c) 2025 18874studentvgu
+##
+## Permission is hereby granted, free of charge, to any person obtaining a copy
+## of this software and associated documentation files (the "Software"), to deal
+## in the Software without restriction, including without limitation the rights
+## to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+## copies of the Software, and to permit persons to whom the Software is
+## furnished to do so, subject to the following conditions:
+##
+## The above copyright notice and this permission notice shall be included in all
+## copies or substantial portions of the Software.
+##
+## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+## SOFTWARE.
+
+
+# if $TERM not set, bypass this enitirely
+[ -n "$TERM" ] 	&& { 
+LOGO="$(cat <<EOF
+█     █▝▀▀▀▀▙▐▌    ▐▌
+█     █      ▐▌    ▐▌
+▜▄   ▄▚   ▀▀█▐▌    ▐▌
+ ▝▜▄▛▘▜▄▄▄▄▄▛▝▙▄▄▄▄▟▘
+EOF
+)"
+
+HBAR="$(cat <<EOF
+───────────────────────
+EOF
+)"
+
+# when connecting via x2g, tput'd implode and somehow take the x2g session with it :(
+# so no `tput cols` for now (until I find a way to safely $TERM to appease it) :((
+# UPDATE: for whatever reason $TERM was not transfered over to the sub shells, causing errors
+if [ "LANG" != "C" ] && [ "${COLUMNS:-"$(TERM="$TERM" tput cols)"}" -gt 28 ]; then
+    echo "╭$HBAR╮"
+    echo "$LOGO" | while IFS='' read -r line; do
+        printf '│ \033[38;5;208m%s\033[0m │\n' "$line"
+    done
+    printf '│ \033[1m%21s\033[0m │\n│ \033[1m%21s\033[0m │\n'\
+        "Vietnamese-German" "University"
+    echo "╰$HBAR╯"
+else
+    printf '%s\n  \033[1m\033[38;5;208m%s\033[0m\n%s\n'\
+        "=======" "VGU" "======="
+fi
+printf '\033[2m(something broke again? come yell @ 18874)\033[0m\n'
+
+printf '\nYou have connected to: \033[4m%s\033[0m\n\n' "$HOSTNAME"
+
+random_greeting(){
+    shuf -n 1 <<'EOF'
+Gutten Tag, $USER!
+Welcome, $USER.
+$USER has logged in.
+Endfie- whoops wrong greeting.
+Today is $(date)... In case you wanna know, $USER.
+Hello $USER.
+$USER, how's that report going?
+$USER! The IC is designing!!!
+Goodluck with the circuitry $USER.
+Is today Friday?
+Take it easy, $USER.
+oh hi
+EOF
+}
+
+make_ps1_fancy(){
+    # modified from https://www.cyberciti.biz/tips/howto-linux-unix-bash-shell-setup-prompt.html
+    # normal
+    # 🗁 (folder unicode char) might cause issues, remove if broken
+    PS1='[\[\033[1m\]\u\[\033[38;5;208m\]@\[\033[39m\]\h \[\033[2m\033[4m\]\W\[\033[0m\]]\[\033[38;5;208m\033[1m\]\$\[\033[0m\] '
+}
+
+
+# Bells and whistles, comment out if server slows down
+if [ "$LANG" != "C" ]; then
+    # to remove formating, comment this line out
+    make_ps1_fancy
+fi
+
+# run `check_quota` in a subshell
+( check_quota.sh; )
+
+# technically I can just use -e... but posix portability :(
+eval echo '"«'"$(TERM="$TERM" random_greeting)"'»"'
+}
